6586c9ecfe18e53402e5a0b600d6275a70c0eaa1
daragmas <daragmas@gmail.com>
2022-07-06 13:06:59 -0400

    commit tree 3071b27d95193b0d6d6d74251e87b0ccfae8a494

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+let findMatching = (array, name) =>{
+    return array.filter((val)=>(val==name)||(val==name.toLowerCase()))
+}
+
+let fuzzyMatch = (array, name)=>{
+    return array.filter((val)=>val[0]==name[0])
+}
+
+let matchName = (array,string)=>{
+    return array.filter((obj)=>obj.name==string)
+
+}
